664f21d311c429138e6881bdb8e0bb5720c5b45b
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-04 21:37:52 +0000

    Add functionality for bag items with a price

--- bag/views.py
+++ bag/views.py
@@ -20,6 +20,5 @@ def add_to_mybag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
 # Create your views here.
