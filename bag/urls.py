09bd20c93d62248a54362e4102b6ce01a81a645d
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-08 17:23:41 +0000

    Add adjust and remove functionality for products

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name= 'view_bag'),
     path('add/<item_id>/', views.add_to_mybag, name= 'add_to_mybag'),
+    path('adjust/<item_id>/', views.adjust_mybag, name= 'adjust_mybag'),
+    path('remove/<item_id>/', views.remove_from_mybag, name= 'remove_from_mybag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -40,3 +43,65 @@ def add_to_mybag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 # Create your views here.
+
+def adjust_mybag(request, item_id):
+    """ Adjust the quantity of the specified product to the shopping bag """
+    
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    warranty = None
+    if 'product_warranty' in request.POST:
+        warranty = request.POST['product_warranty']
+    bag = request.session.get('bag', {})
+
+    if warranty:
+        if quantity > 0:
+            bag[item_id]['items_by_warranty'][warranty] = quantity
+            messages.success(
+                    request, f'Updated warranty {warranty.upper()} {product.name} quantity to {bag[item_id]["items_by_warranty"][warranty]}')
+        else:
+            del bag[item_id]['items_by_warranty'][warranty]
+            if not bag[item_id]['items_by_warranty']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed warranty {warranty.upper()} {product.name} from your bag')
+            
+        
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+            
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_mybag(request, item_id):
+    """ Remove the specified product to the shopping bag """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        warranty = None
+        if 'product_warranty' in request.POST:
+            warranty = request.POST['product_warranty']
+        bag = request.session.get('bag', {})
+
+        if warranty:
+            del bag[item_id]['items_by_warranty'][warranty]
+            if not bag[item_id]['items_by_warranty']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed warranty {warranty.upper()} {product.name} from your bag')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
