f0e9e434855da574cc6c66b77ebf19b70e1ed785
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-18 22:22:39 +0000

    Add chceckout style for my form

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KJLsKCbBVbCYpEHIRdhd2LEEn6prXQbInMGuppiqWPBQlAeWhbeR9YFSZpovzIcsj78FThTS2KA04mDuceOU0sM00utmOddrX',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
