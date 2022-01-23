e5de69fdb17f16307e65162f754862b28f144cea
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-23 18:49:42 +0000

    Upgrade profile functionality

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,8 @@ from bag.contexts import bag_contents
 
 import stripe
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 import json
 
 @require_POST
@@ -101,7 +103,27 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
+        
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
