43a84b5127af3a0731d11934eeed4f96184efe53
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-26 15:00:40 +0000

    Add Procfile

--- eshop/settings.py
+++ eshop/settings.py
@@ -121,12 +121,21 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    print("using Postgres DB")
+
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    print("using SQLite3 DB")
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }}
+
 
 
 # Password validation
