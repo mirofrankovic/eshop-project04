07bcce0753ebee505e36de809783cf29928c3a47
Miro Frankovic <miro.frankovic@gmail.com>
2022-01-28 00:06:37 +0000

    Add test to the product model

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class TestProductModel(TestCase):
+
+    def test_create_product(self):
+        product = Product(name="CocaCola", description='Some test content description.')
+        self.assertEqual(product.name, 'CocaCola')
+        self.assertEqual(product.description, "Some test content description.")
+        self.assertEqual(product.year_warranty, '1');
